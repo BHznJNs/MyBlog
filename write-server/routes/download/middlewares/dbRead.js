aa51ede8c640f0fc7321b21aec4c7354a4327908
BHznJNs <441768875@qq.com>
2022-12-22 16:25:51 +0800

    write-server bug fixed

--- write-server/routes/download/middlewares/dbRead.js
+++ write-server/routes/download/middlewares/dbRead.js
@@ -6,9 +6,9 @@ function dbRead(req, res, next) {
     const name = req.body.name
     globalThis.DB.read(name, (err, content) => {
         if (err) {
-            // 数据库写入出错
+            // 数据库读取出错
             console.warn(err)
-            res.send(dbWriteErrMsg)
+            res.send(dbReadErrMsg)
         } else { //         server ver: *** .json
             res.locals.noteContent = content
             next()
